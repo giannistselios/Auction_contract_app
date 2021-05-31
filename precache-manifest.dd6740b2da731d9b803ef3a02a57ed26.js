c082785aee86eaed0957f5d51bf08aa7221856f9
giannistselios <giannis_tselios1999@hotmail.com>
2021-05-31 19:39:57 +0300

    Updates

---
+++ precache-manifest.dd6740b2da731d9b803ef3a02a57ed26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1e353ad262bd323fa1d08635ac7b19",
+    "url": "/Auction_contract_app/index.html"
+  },
+  {
+    "revision": "7077da1957d78dc33896",
+    "url": "/Auction_contract_app/static/css/main.fd81253e.chunk.css"
+  },
+  {
+    "revision": "75672f937a17b2c0aa25",
+    "url": "/Auction_contract_app/static/js/2.d7b0bc5d.chunk.js"
+  },
+  {
+    "revision": "7077da1957d78dc33896",
+    "url": "/Auction_contract_app/static/js/main.1e9412df.chunk.js"
+  },
+  {
+    "revision": "019e70a23a6191b68e6c",
+    "url": "/Auction_contract_app/static/js/runtime-main.b46b7066.js"
+  }
+]);
